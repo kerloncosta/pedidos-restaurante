c5882bdb62fcdb3a0a40a9a2a1a2959c9abff0d0
kerloncosta <kerloncosta6@gmail.com>
2026-06-15 14:16:57 -0300

    feat: add attributes and constructors to Lanche class

--- src/main/java/ifrs/ibiruba/Lanche.java
+++ src/main/java/ifrs/ibiruba/Lanche.java
@@ -1,10 +1,23 @@
 package ifrs.ibiruba;
 
 public class Lanche extends ItemCardapio{
+    private String size = "U";
+    private boolean containsAdditional = false;
+
+    public String getSize() {return size;}
+    public boolean isContainsAdditional() {return containsAdditional;}
+
+    public Lanche(String name, double price, String size, boolean containsAdditional, String category) {
+        super(name, price, category);
+        this.size = size;
+        this.containsAdditional = containsAdditional;
+
+    }
 
     public Lanche(String name, double price, String category) {
         super(name, price, category);
     }
+
 
     @Override
     public double calculateFinalPrice() {
