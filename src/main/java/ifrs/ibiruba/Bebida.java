7f933446f59cba35e919b40b437e2f06c14626be
kerloncosta <kerloncosta6@gmail.com>
2026-06-15 12:50:03 -0300

    feat: add constructors and exclusive attributes to Bebida class

--- src/main/java/ifrs/ibiruba/Bebida.java
+++ src/main/java/ifrs/ibiruba/Bebida.java
@@ -1,9 +1,30 @@
 package ifrs.ibiruba;
 
 public class Bebida extends ItemCardapio{
+    private String flavor;
+    private String subcategory;
+    private boolean isReturnable;
+    private boolean returnedBottle;
 
-    public Bebida(String name, double price, String category) {
+    public String getFlavor() {return flavor;}
+    public String getSubcategory() {return subcategory;}
+    public boolean isReturnable() {return isReturnable;}
+    public boolean isReturnedBottle() {return returnedBottle;}
+
+    public Bebida(String name, double price, String category, String subcategory, String flavor, boolean isReturnable, boolean returnedBottle) {
         super(name, price, category);
+        this.subcategory = subcategory;
+        this.flavor = flavor;
+        this.isReturnable = isReturnable;
+        this.returnedBottle = returnedBottle;
+    }
+
+    public Bebida(String name, double price, String category, String subcategory) {
+        super(name, price, category);
+        this.subcategory = subcategory;
+        this.flavor = "";
+        this.isReturnable = false;
+        this.returnedBottle = false;
     }
 
     @Override
