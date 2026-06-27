023989179b1747475c117af40d5a0d23ad16a297
kerloncosta <kerloncosta6@gmail.com>
2026-06-27 19:12:22 -0300

    refactor: update Lanche class to use dynamic pricing based on size

--- src/main/java/ifrs/ibiruba/Lanche.java
+++ src/main/java/ifrs/ibiruba/Lanche.java
@@ -2,50 +2,34 @@ package ifrs.ibiruba;
 
 public class Lanche extends ItemCardapio{
     private String size = "U";
-    private boolean containsAdditional = false;
 
     public String getSize() {return size;}
-    public boolean isContainsAdditional() {return containsAdditional;}
 
-    public Lanche(String name, double price, String size, boolean containsAdditional, String category) {
+    public Lanche(String name, double price, String size, String category) {
         super(name, price, category);
         this.size = size;
-        this.containsAdditional = containsAdditional;
-
     }
 
     public Lanche(String name, double price, String category) {
         super(name, price, category);
     }
 
-
     @Override
     public double calculateFinalPrice() {
         double finalPrice = super.getPrice();
 
-        if (this.containsAdditional) {
-            if ("G".equalsIgnoreCase(this.size)) {
-                finalPrice += 7.00;
-            } else if ("P".equalsIgnoreCase(this.size)) {
-                finalPrice += 3.00;
-            } else {
-                finalPrice += 5.00;
-            }
+        if("G".equalsIgnoreCase(size)){
+            finalPrice += 10.00;
+        }else if("P".equalsIgnoreCase(size)){
+            finalPrice -= 5;
         }
+
         return finalPrice;
     }
 
     @Override
     public String toString() {
-
-        String descricao = getName() + " (" + this.size + ")";
-
-        if (this.containsAdditional) {
-            descricao += " com cheddar e bacon";
-        }
-
-        descricao += " - R$ " + String.format("%.2f", calculateFinalPrice());
-
-        return descricao;
+        return getName() + " (" + this.size + ") - R$ " +
+                String.format("%.2f", calculateFinalPrice());
     }
 }
