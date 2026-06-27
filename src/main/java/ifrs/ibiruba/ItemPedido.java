22326f1105995cc594ef41d2c4eb0f2966aacf16
kerloncosta <kerloncosta6@gmail.com>
2026-06-27 18:55:43 -0300

    feat: create ItemPedido class

---
+++ src/main/java/ifrs/ibiruba/ItemPedido.java
@@ -0,0 +1,20 @@
+package ifrs.ibiruba;
+
+public class ItemPedido {
+    private ItemCardapio item;
+    private int amount;
+
+    public ItemPedido(ItemCardapio item, int amount) {
+        this.item = item;
+        this.amount = amount;
+    }
+
+    public double calculateSubtotal(){
+        return item.calculateFinalPrice() *  amount;
+    }
+
+    @Override
+    public String toString() {
+        return amount + "x " + item.toString() + " = Subtotal: " + String.format("%.2f",calculateSubtotal());
+    }
+}
