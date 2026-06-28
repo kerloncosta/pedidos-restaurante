c428e1b907c09c917e6cb7aca82ba6d09a1f1cb0
kerloncosta <kerloncosta6@gmail.com>
2026-06-27 22:16:21 -0300

    feat: implement loadMenu and showMenu using TreeMap in Cardapio

--- src/main/java/ifrs/ibiruba/Cardapio.java
+++ src/main/java/ifrs/ibiruba/Cardapio.java
@@ -18,7 +18,45 @@ public class Cardapio {
         return itens.get(code);
     }
 
+    public void showMenu(){
+        System.out.println("\n|================= Cardápio ================|");
+
+        for (Map.Entry<Integer, ItemCardapio> item : itens.entrySet()) {
+            System.out.println("| " + item.getKey() + " - " + item.getValue());
+        }
+
+        System.out.println("|=============================================|");
+    }
+
     private void loadMenu(){
 
+        addItens(1, new Lanche("Xis Salada", 22.00, "Xis"));
+        addItens(2, new Lanche("Xis Frango", 24.00, "Xis"));
+        addItens(3, new Lanche("Xis Entreveiro", 28.00, "Xis"));
+        addItens(4, new Lanche("Xis Bacon", 26.00, "Xis"));
+        addItens(5, new Lanche("Xis Especial", 26.00, "Xis"));
+
+        addItens(6, new Lanche("Pizza de Calabresa", 38.00, "Pizza"));
+        addItens(7, new Lanche("Pizza de Quatro Queijos", 42.00, "Pizza"));
+        addItens(8, new Lanche("Pizza de Strogonoff", 35.00, "Pizza"));
+        addItens(9, new Lanche("Pizza de Coração", 44.00, "Pizza"));
+        addItens(10, new Lanche("Pizza de Chocolate", 40.00, "Pizza"));
+
+        addItens(11, new Lanche("Batata Frita Simples", 18.00, "Porções"));
+        addItens(12, new Lanche("Batata Frita c/ Catupiry", 22.00, "Porções"));
+        addItens(13, new Lanche("Polenta Frita", 16.00, "Porções"));
+        addItens(14, new Lanche("Anéis de Cebola", 19.00, "Porções"));
+        addItens(15, new Lanche("Frango a Passarinho", 29.00, "Porções"));
+
+        addItens(16, new Bebida("Água Mineral Natural", 2.50, "Sem álcool", "Água"));
+        addItens(17, new Bebida("Água Mineral com Gás", 3.00, "Sem álcool", "Água"));
+        addItens(18, new Bebida("Refrigerante Lata", 4.50, "Sem álcool", "Refri", "", false, false));
+        addItens(19, new Bebida("Refrigerante 1L", 8.00, "Sem álcool", "Refri", "", true, false));
+        addItens(20, new Bebida("Refrigerante 2L", 10.00, "Sem álcool", "Refri", "", true, false));
+        addItens(21, new Bebida("Cerveja Polar 600ml", 14.00, "Alcoólica", "Cerveja", "", true, false));
+        addItens(22, new Bebida("Cerveja Brahma 600ml", 13.00, "Alcoólica", "Cerveja", "", true, false));
+        addItens(23, new Bebida("Heineken Long Neck", 12.00, "Alcoólica", "Cerveja", "", false, false));
+        addItens(24, new Bebida("Skol Litrão", 16.00, "Alcoólica", "Cerveja", "", true, false));
+        addItens(25, new Bebida("Torre de Chopp 2,5L", 85.00, "Alcoólica", "Chopp", "", false, false));
     }
 }
