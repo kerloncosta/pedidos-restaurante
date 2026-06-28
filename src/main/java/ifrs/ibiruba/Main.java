c6b7b274116b61d5ebbb3e7873a5b4ffb13cb8ca
kerloncosta <kerloncosta6@gmail.com>
2026-06-28 00:30:18 -0300

    feat: setup Main class variables and switch skeleton

--- src/main/java/ifrs/ibiruba/Main.java
+++ src/main/java/ifrs/ibiruba/Main.java
@@ -1,10 +1,46 @@
 package ifrs.ibiruba;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
 
+        ArrayList<Pedido> pedidos = new ArrayList<>();
+        Scanner keyboard = new Scanner(System.in);
+        Cardapio menu = new Cardapio();
 
+        int option, re = -1;
+        boolean isReturnable = false;
+        boolean returnedBottle = false;
+
+        do {
+            System.out.println("0 - Sair");
+            System.out.println("1 - Fazer Pedido");
+            System.out.println("2 - Conferir últimos Pedidos");
+            System.out.print("Escolha uma opção: ");
+            option = keyboard.nextInt();
+
+            switch (option) {
+
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+
+                case 1:
+
+                    break;
+
+                case 2:
+
+                    break;
+
+                default:
+
+                    break;
+            }
+        }while(option != 0);
     }
 }
