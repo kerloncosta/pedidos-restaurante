2225f2e5d5fae421dc63ea19ab5388c5ba16ac77
kerloncosta <kerloncosta6@gmail.com>
2026-06-28 00:00:07 -0300

    feat: implement item addition, total calculation, and receipt printing in Pedido

--- src/main/java/ifrs/ibiruba/Pedido.java
+++ src/main/java/ifrs/ibiruba/Pedido.java
@@ -24,5 +24,33 @@ public class Pedido {
         Random rand = new Random();
         this.noteNumber = "#" + String.format("%04d", rand.nextInt(1500) + 1);
     }
+
+    public void addItem(ItemCardapio item, int amount){
+        ItemPedido newItem = new ItemPedido(item, amount);
+        itens.add(newItem);
+    }
+
+    public double calculateTotal(){
+        double total = 0.0;
+
+        for(ItemPedido item : itens){
+            total += item.calculateSubtotal();
+        }
+
+        return total;
+    }
+
+    public void printSummary(){
+        System.out.println("--------------------------Resumo pedido--------------------------");
+        System.out.println("N° Comanda: " + this.noteNumber + "   Data: " + this.issueDate + "   Cliete: " + this.nameClient);
+        System.out.println("-----------------------------------------------------------------");
+
+        for (ItemPedido it : itens){
+            System.out.println(it);
+        }
+
+        System.out.println("-----------------------------------------------------------------");
+        System.out.println("Total a pagar: R$ " + String.format("%.2f", calculateTotal()));
+    }
 }
 
